0d7230898025b026e07325981e0a8a9709f58e4f
santy <santy@DESKTOP-7KIGB8O>
2024-11-03 17:37:05 -0500

    added footer to buy page

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import { BuyComponent } from "@/features/buy-page";
+import { Footer } from "@/features/footer";
 
 const page = () => {
     return (
         <main className={styles.page}>
             <BuyComponent />
+            <Footer />
         </main>
     );
 };
