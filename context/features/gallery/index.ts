e5c1fa062b26e88047715a4addff45ceb7c1ec67
santy <santy@DESKTOP-7KIGB8O.localdomain>
2025-12-11 18:11:08 -0500

    added gallery

--- context/features/gallery/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Gallery } from "./components/gallery/Gallery";
-export { default as LogoHeading } from "./components/logo-heading/LogoHeading";
-export { default as LoadingScreen } from "./components/loading-screen/LoadingScreen";
-export { backArrow, instagramColor } from "./lib/icons";
--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,9 +1,17 @@
-import { defineLive } from 'next-sanity'
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({
-  client: client.withConfig({
-    apiVersion: 'vX',
-  }),
-})
+type FetchArgs<TParams = Record<string, unknown>> = {
+  query: string
+  params?: TParams
+}
 
+export async function sanityFetch<T = unknown, TParams = Record<string, unknown>>(
+  args: FetchArgs<TParams>
+): Promise<{ data: T }> {
+  const data = await client.fetch<T>(args.query, args.params)
+  return { data }
+}
+
+export function SanityLive() {
+  return null
+}
